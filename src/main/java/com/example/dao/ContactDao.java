e6c41d4eb9d006789f41673bb0ff7607cb5a8881
Rajdeep Dua <dua_rajdeep@yahoo.com>
2016-02-16 12:19:43 +0530

    contactdao

--- src/main/java/com/example/dao/ContactDao.java
+++ src/main/java/com/example/dao/ContactDao.java
@@ -3,12 +3,20 @@ package com.example.dao;
 import com.example.model.Contact;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 @Repository
 public interface ContactDao extends CrudRepository<Contact, Long> {
 
+    @Query(value = "SELECT * FROM salesforce.contact t WHERE t.lastname = 'lastname'",
+            nativeQuery=true
+    )
     public List<Contact> findByLast(String lastName);
+
+    @Query(value = "SELECT * FROM salesforce.contact t WHERE t.id = 'id'",
+            nativeQuery=true
+    )
     public List<Contact> findById(Integer id);
 }
